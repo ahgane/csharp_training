f9ef04e652bdb1f84d690b49839882f967817aea
ahgane <pochtagane@gmail.com>
2018-06-24 04:06:01 +0300

    Trying 'if esle' Sales test

---
+++ Addressbook-web-tests/Addressbook-web-tests/Z_If then Sales/Sales.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WebAddressbookTests.tests
+{
+    [TestClass]
+    public class Sales
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            double total = 900;
+            bool VipClient = false;
+
+            if (total > 1000 || VipClient)
+                {
+                total = total * 0.9;
+                System.Console.Out.Write("Скидка 10%, общая сумма " + total);
+            }
+            else
+            {
+                System.Console.Out.Write("Хорошего дня! Общая сумма " + total);
+            }
+        }
+    }
+}
